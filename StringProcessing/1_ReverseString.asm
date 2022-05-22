34a5141220b30ecf9a41a3608c661013fa37666a
alaminkawsar <alaminkawsar8643@gmail.com>
2022-05-23 00:55:17 +0600

    case change in a string

--- StringProcessing/1_ReverseString.asm
+++ StringProcessing/1_ReverseString.asm
@@ -1,13 +1,59 @@
+.MODEL SMALL
 
-; You may customize this and other start-up templates; 
-; The location of this template is c:\emu8086\inc\0_com_template.txt
+.STACK 100H
 
-org 100h
+.DATA
 
-; add your code here
+VAR1 DB 40 DUP(?)
+VAR2 DB ?
+NEWLINE DB 0AH,0DH,'$'
+.CODE
 
-ret
-
+MAIN PROC
+    mov ax,@DATA
+    mov ds,ax
+    
+    MOV AH,1
+    MOV SI,0
+    GETSTRING:
+        MOV AH,1
+        INT 21H
+        CMP AL,0DH
+        JE END_GETSTRING
+        MOV VAR1+SI,AL
+       
+        INC SI
+        JMP GETSTRING
+    
+    END_GETSTRING:
+    MOV VAR1+SI,'$'
+    CALL PRINTLINE
+    
+    MOV CX,SI
+    MOV AH,2
+    PRINTSTRING:
+        DEC SI
+        MOV  DL, VAR1[SI]
+        INT 21H   
+    LOOP PRINTSTRING
+   
+    MOV AH,4CH
+    INT 21H
+    
+    MAIN ENDP 
+    
+    PRINTLINE PROC
+        PUSH DX
+        PUSH AX
+        LEA DX,NEWLINE
+        MOV AH,9
+        INT 21H
+        POP AX
+        POP DX
+        RET
+        PRINTLINE ENDP 
+        
+END MAIN
 
 
 
